1b0ad1e887c960bf0fa512e2443e65f79b1f85bb
Aashish rana <aashishrana71135@gmail>
2022-10-01 19:34:00 -0700

    Arrow function

---
+++ ArrowFunction.js
@@ -0,0 +1,14 @@
+let greet = (user) => {
+    console.log("Hello", user);
+}
+
+console.log(greet("Rana"));
+
+
+
+let add = (num1, num2) => 
+    num1 + num2;
+
+
+let result = add(5,6);
+console.log(result);
