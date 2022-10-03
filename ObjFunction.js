3dd716b398962d500d524eb4777be2ffd448a347
Aashish rana <aashishrana71135@gmail>
2022-10-02 18:36:12 -0700

    Object function comparision

--- ObjFunction.js
+++ ObjFunction.js
@@ -1,14 +1,35 @@
 let Laptop = {
-    cpu: 'i9',
+    cpu: 'i5',
     ram: 16,
     brand: "HP",
 
     greet: function() {
+        let storage = 100
+        console.log(storage);
         console.log("Hello world");
     },
 
     getConfig: function() {
-        console.log(Laptop.brand);
+        console.log(this.cpu);
+    },
+
+    
+
+}
+
+let Laptop2 = {
+    cpu: 'i7',
+    ram: 16,
+    brand: "HP",
+
+    greet: function() {
+        let storage = 100
+        console.log(storage);
+        console.log("Hello world");
+    },
+
+    getConfig: function() {
+        console.log(this.cpu);
     },
 
     
@@ -16,9 +37,18 @@ let Laptop = {
 }
 
 Laptop.greet();
-Laptop.greet();
+Laptop2.greet();
 
 Laptop.getConfig();
+Laptop2.getConfig();
+
+if(Laptop.cpu > Laptop2.cpu) {
+    console.log(Laptop);
+}
+
+else {
+    console.log(Laptop2);
+}
 
 
 
