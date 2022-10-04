bc97a9d05dd1ac0a7ba666f6e025063741d6f19a
Aashish rana <aashishrana71135@gmail>
2022-10-03 19:16:10 -0700

    set in arraay

---
+++ set.js
@@ -0,0 +1,16 @@
+// In sets we don't have repeated values
+
+
+let nums = new Set("bookmarks");
+console.log(nums);
+
+let nums1 = new Set();
+
+nums1.add(2);
+nums1.add(3);
+nums1.add(4);
+nums1.add(3);
+nums1.add("abhay");
+nums1.add("Abhay");
+nums1.add("Abhay");
+console.log(nums1);
